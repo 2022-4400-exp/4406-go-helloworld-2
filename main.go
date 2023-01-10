c27ffee981c8338c04ebb8d31637f333bf9af83c
m19016 <m19016@g.metro-cit.ac.jp>
2023-01-10 16:17:55 +0900

    unko

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4406: はろー ver.2")
+        return c.String(http.StatusOK, "4406: wiiiiiiiiiii")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4406", "message": "aaaaaaaaaaaa"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4406", "message": "waaaaaaaa"})
     }
 }
